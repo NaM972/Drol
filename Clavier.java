577e881e8eb902f5c027f92a084533e4a8f384c5
root <root@Serveur.lan>
2019-04-01 13:55:09 +0200

    first commit

---
+++ Clavier.java
@@ -0,0 +1,13 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Clavier extends Thread{
+
+  public Clavier(JPanel j){
+    //j.addKeyListener(this);
+  }
+
+
+
+}
